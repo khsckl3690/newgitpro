51debd828b46c41934e8e66cf6ebb5b3b64c3fea
khsckl3690 <129178582+khsckl3690@users.noreply.github.com>
2023-09-21 17:46:13 +0900

    try practice&debug
    practiced try statement and fixed the bug in the last commit.

--- PythonApplication1/PythonApplication1/PythonApplication1.py
+++ PythonApplication1/PythonApplication1/PythonApplication1.py
@@ -2,6 +2,16 @@ class MyClass:
     age=17
     job='student'
     
+    try:
+        @classmethod
+        def t(cls):
+            jkld
+            
+    except Exception:
+        print("qwerty")
+        
+
+
     def __init__(self, M, D):
         self.birthday=(M, D)
         
@@ -9,10 +19,11 @@ class MyClass:
         self.p=a
         
     def __repr__(self):
-        print(self.p)
+        return self.p
         
 K=MyClass(3, 14)
 print(f"K's age is {K.age}, and K's job is {K.job}")
 print(f"K's birthday is {K.birthday[0]}.{K.birthday[1]}")
 K.app('app')
 print(K)
+K.t
